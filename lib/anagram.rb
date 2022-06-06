43ba3a553a60e1aef138bae9009ff9dae3bdf333
Luke Baltay <luke.baltay@yale.edu>
2022-06-06 15:18:16 -0400

    commit tree e2e911178f8f666730a1fbba215f366a604cc39e

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+
+    attr_reader :word, :word_array
+
+    def initialize(word)
+        @word=word
+    end
+
+    def match(word_array)
+        @word_array=word_array
+        self.anagram?
+
+    end
+
+    def letter_array(prompt)
+        letters = []
+        prompt.each_char{|char| letters << char}
+        letters.sort
+    end
+
+    def anagram?
+        @word_array.select {|prompt| letter_array(prompt) == letter_array(@word)}
+    end
+
+    # binding.pry
+end
+
